15b053f5fa21fcecf4202f6f201af05513681a1a
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2026-05-13 16:31:19 -0700

    Format date.js for improved readability

--- scripts/date.js
+++ scripts/date.js
@@ -1,4 +1,5 @@
-document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("year").textContent =
+new Date().getFullYear();
 
 document.getElementById("lastModified").textContent =
-  "Last Modified: " + document.lastModified;
+"Last Modified: " + document.lastModified;
