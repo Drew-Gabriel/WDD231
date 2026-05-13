4e328216ecb5bc12a5f3e4f17cc0d972c6fd1b86
Drew-Gabriel <126345558+Drew-Gabriel@users.noreply.github.com>
2026-05-13 15:05:10 -0700

    Update date.js

--- scripts/date.js
+++ scripts/date.js
@@ -1,1 +1,6 @@
+// Current year
+document.getElementById("year").textContent = new Date().getFullYear();
 
+// Last modified date
+document.getElementById("lastModified").textContent =
+  `Last Modified: ${document.lastModified}`;
